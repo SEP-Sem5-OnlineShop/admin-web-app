2e8f5016a1d461780851e1ce80f392c1f378179b
Darsahan Sandaruwan <dsandaruwan7@gmail.com>
2021-09-10 11:37:49 +0530

    partially done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -49,6 +49,16 @@ export default function Sidebar() {
                                     Vendor List
                                 </NavLink>
                             </li>
+                            <li className="rounded-lg mb-2 ">
+                                <NavLink
+                                    to="/vendor/register"
+                                    className="flex items-center gap-4 text-sm text-gray-700 font-light px-4 py-3 rounded-lg"
+                                    activeClassName="bg-gradient-to-tr from-light-blue-500 to-light-blue-700 text-white shadow-md"
+                                >
+                                    <Icon name="toc" size="2xl" />
+                                    Vendor Register
+                                </NavLink>
+                            </li>
                         </ul>
                     </div>
                 </div>
