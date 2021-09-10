81be47362e34935332928580e6a13699f656baf8
Darsahan Sandaruwan <dsandaruwan7@gmail.com>
2021-09-10 06:12:30 +0530

    unwanted fields are removed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,8 +6,9 @@ import TrafficCard from 'components/TrafficCard';
 
 export default function Dashboard() {
     return (
-        <>
-            <div className="bg-light-blue-500 px-3 md:px-8 h-40" />
+        <div className="p-4">
+            <h2 class="text-xl">Welcome To The OnTheWay Admin Panel</h2>
+            {/* <div className="bg-light-blue-500 px-3 md:px-8 h-40" />
 
             <div className="px-3 md:px-8 -mt-24">
                 <div className="container mx-auto max-w-full">
@@ -67,9 +68,9 @@ export default function Dashboard() {
                         />
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-            <div className="px-3 md:px-8 h-auto">
+            {/* <div className="px-3 md:px-8 h-auto">
                 <div className="container mx-auto max-w-full">
                     <div className="grid grid-cols-1 xl:grid-cols-5">
                         <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14">
@@ -80,7 +81,7 @@ export default function Dashboard() {
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
+            </div> */}
+        </div>
     );
 }
