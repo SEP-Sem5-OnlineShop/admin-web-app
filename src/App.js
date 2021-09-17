4a67b1d3b998957841709bc324c029efc521082c
jamini8 <jamini.18@cse.mrt.ac.lk>
2021-09-17 16:36:50 +0530

    partially done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+
+
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Sidebar from 'components/Sidebar';
 import Dashboard from 'pages/Dashboard';
@@ -15,26 +19,91 @@ import Footer from 'components/Footer';
 // Tailwind CSS Style Sheet
 import 'assets/styles/tailwind.css';
 
-function App() {
-    return (
-        <>
-            <Sidebar />
-            <div className="md:ml-64">
-                <div style={{minHeight: 'calc(100vh - 169px)'}}>
-                    <Switch>
-                        <Route exact path="/" component={Dashboard} />
-                        <Route exact path="/settings" component={Settings} />
-                        <Route exact path="/tables" component={Tables} />
-                        <Route exact path="/singleVendor" component={singleVendor} />
-                        <Route exact path="/requestList" component={requestList} />
-                        <Route exact path="/maps" component={Maps} />
+// function App() {
+//     return (
+//         <>
+//             <Sidebar />
+//             <div className="md:ml-64">
+//                 <div style={{minHeight: 'calc(100vh - 169px)'}}>
+//                     <Switch>
+//                         <Route exact path="/" component={Dashboard} />
+//                         <Route exact path="/settings" component={Settings} />
+//                         <Route exact path="/tables" component={Tables} />
+//                         <Route exact path="/singleVendor" component={singleVendor} />
+//                         <Route exact path="/requestList" component={requestList} />
+//                         <Route exact path="/maps" component={Maps} />
+//                         <Redirect from="*" to="/" />
+//                     </Switch>
+//                 </div>
+//                 <Footer />
+//             </div>
+//         </>
+//     );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:''};
+
+  }
+  callApi(){
+    fetch("http://localhost:9000/testApi")
+    .then(res =>res.text())
+    .then(res => this.setState({apiResponse:res}));
+  }
+
+  componentWillMount(){
+    this.callApi();
+  }
+
+
+
+render(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+       
+        
+      </header>
+      
+
+      <Sidebar />
+             <div className="md:ml-64">
+                 <div style={{minHeight: 'calc(100vh - 169px)'}}>
+                     <Switch>
+                         <Route exact path="/" component={Dashboard} />
+                         <Route exact path="/settings" component={Settings} />
+                         <Route exact path="/tables" component={Tables} />
+                         <Route exact path="/singleVendor" component={singleVendor} />
+                         <Route exact path="/requestList" component={requestList} />
+                         <Route exact path="/maps" component={Maps} />
                         <Redirect from="*" to="/" />
-                    </Switch>
-                </div>
-                <Footer />
-            </div>
-        </>
-    );
+                     </Switch>
+                 </div>
+
+                 <p>{this.state.apiResponse}</p>
+                 
+                 <Footer />
+             </div>
+
+
+
+
+    </div>
+
+    
+  );
+}
 }
 
 export default App;
