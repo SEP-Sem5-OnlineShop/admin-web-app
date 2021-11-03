d3d4fa6a9c109b0831d6ee73f1b87f02ffec71c4
jamini8 <jamini.18@cse.mrt.ac.lk>
2021-11-03 10:10:44 +0530

    finished

--- src/components/chart/ChartLine.js
+++ src/components/chart/ChartLine.js
@@ -14,9 +14,9 @@ export default function ChartLine() {
     let day4 = new Date(day1);
     let day5 = new Date(day1);
     day2.setDate(day2.getDate() - 1);
-    day3.setDate(day3.getDate() - 1);
-    day4.setDate(day4.getDate() - 1);
-    day5.setDate(day5.getDate() - 1);
+    day3.setDate(day3.getDate() - 2);
+    day4.setDate(day4.getDate() - 3);
+    day5.setDate(day5.getDate() - 4);
     console.log(day2.toDateString());
     console.log(day1.toDateString());
 
